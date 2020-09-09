6cffc8ab665fda7b075221eca6e2d185ac1c1fee
Ivangyurcovits <68866090+Ivangyurcovits@users.noreply.github.com>
2020-09-09 14:14:28 -0300

    Vivero 8
    Calificación con estrellas

--- ver-planta.js
+++ ver-planta.js
@@ -1,31 +1,36 @@
 var plantasArray = [];
-var commentsArray =[];
+var commentsArray = [];
 
-function showPlantas(array, comentarioArray){
+function showPlantas(array, comentarioArray) {
     let verPlanta = JSON.parse(localStorage.getItem('planta'));
-    for (let i = 0; i < plantasArray.length; i++){
+    for (let i = 0; i < plantasArray.length; i++) {
         let planta = array[i];
         let info = "";
         let imgs = "";
         let comments = "<hr>";
-        if (planta.id == verPlanta.plantaId){
+        if (planta.id == verPlanta.plantaId) {
             info += '<h3>' + planta.nombre + '</h3>';
             info += '<strong>Origen: ' + planta.origen + '</strong><br>';
             info += '<i>' + planta.nomcient + '</i> <br>';
             info += '(' + planta.edad + ' años).<br>';
 
-            imgs += '<img class="img" src= "imgs/'+ planta.nombre +'/1.jpg" height="200px"  HSPACE="10" alt="">';
-            imgs += '<img class="img" src= "imgs/'+ planta.nombre +'/2.jpg" height="200px"  HSPACE="10" alt="">';
-            imgs += '<img class="img" src= "imgs/'+ planta.nombre +'/3.jpg" height="200px"  HSPACE="10" alt="">';
+            imgs += '<img class="img" src= "imgs/' + planta.nombre + '/1.jpg" height="200px"  HSPACE="10" alt="">';
+            imgs += '<img class="img" src= "imgs/' + planta.nombre + '/2.jpg" height="200px"  HSPACE="10" alt="">';
+            imgs += '<img class="img" src= "imgs/' + planta.nombre + '/3.jpg" height="200px"  HSPACE="10" alt="">';
 
-            for (let comment in comentarioArray){
-                if (comentarioArray[comment].id_planta == verPlanta.plantaId){
-                    comments += '<strong>'+ comentarioArray[comment].user +'</strong> dijo: <br>';
-                    comments += '<p>'+ comentarioArray[comment].comentario +'</p><br>';
-                    comments += 'Calificación  <strong>'+ comentarioArray[comment].calificacion +'</strong>/5<br>';
-                    comments += '<br><hr>'
+            commentsArray.forEach(function (comment) {
+                let puntos = "";
+                if (comment.id_planta == verPlanta.plantaId) {
+                    comments += '<strong>'+ comment.user +'</strong> dijo: <br> <p>'+ comment.comentario +'</p>';
+                    for (let i = 1; i <= comment.calificacion; i++) {
+                        puntos += '<span class="fa fa-star checked"></span>';
+                    }
+                    for (let i = comment.calificacion + 1; i <= 5; i++) {
+                        puntos += '<span class="fa fa-star"></span>';
+                    }
+                    comments += '<div style="text-align: right;">'+ puntos +'</div><br><hr>';
                 }
-            }
+            });
 
             document.getElementById("content").innerHTML = info;
             document.getElementById("images").innerHTML = imgs;
@@ -37,13 +42,13 @@ function showPlantas(array, comentarioArray){
 
 
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(COMENTARIOS_URL).then(function(resultObj){
+    getJSONData(COMENTARIOS_URL).then(function (resultObj) {
         if (resultObj.status === "ok") {
-            commentsArray = resultObj.data;            
+            commentsArray = resultObj.data;
         }
     });
-    getJSONData(URL_VIVERO).then(function (resultObj){
-        if (resultObj.status === "ok"){
+    getJSONData(URL_VIVERO).then(function (resultObj) {
+        if (resultObj.status === "ok") {
             plantasArray = resultObj.data;
 
             showPlantas(plantasArray, commentsArray);
